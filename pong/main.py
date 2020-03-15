aaa0dc1d5ef8c2fc0138aaf3bf481f16019cffd1
Augs Machado <augustomachadoramos@gmail.com>
2020-03-15 00:41:56 -0300

    Delete the random method

--- pong/main.py
+++ pong/main.py
@@ -1,9 +1,10 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
+from kivy.properties import (
+    NumericProperty, ReferenceListProperty, ObjectProperty
+)
 from kivy.vector import Vector
 from kivy.clock import Clock
-from random import randint
 
 
 class PongPaddle(Widget):
